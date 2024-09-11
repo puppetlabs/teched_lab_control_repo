aa349df0977b046a2c51ad668a14eaeda7e7b2fc
Jain Waldrip <jain.waldrip@perforce.com>
2024-09-11 12:37:40 -0500

    Add stdlib and include mysql server class

--- Puppetfile
+++ Puppetfile
@@ -2,3 +2,4 @@ forge 'https://forge.puppet.com'
 
 # MySQL Module
 mod 'puppetlabs-mysql', '16.0.0'
+mod 'puppetlabs-stdlib', '9.6.0'
---
+++ site-modules/profile/manifests/mysql_server.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::mysql_server
+class profile::mysql_server {
+  include mysql::server
+}
