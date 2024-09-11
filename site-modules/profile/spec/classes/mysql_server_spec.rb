e17a58011b1827f76efb3cfd6f1e8168e67cf3ad
Jain Waldrip <jain.waldrip@perforce.com>
2024-09-11 13:00:03 -0500

    Configure MySQL server

--- site-modules/profile/spec/classes/mysql_server_spec.rb
+++ site-modules/profile/spec/classes/mysql_server_spec.rb
@@ -9,6 +9,10 @@ describe 'profile::mysql_server' do
 
       it { is_expected.to compile.with_all_deps }
       
+      it "accepts mysql configuration parameters" do
+        is_expected.to contain_profile__mysql_server
+      end
+
       it "configures a mysql server" do
         is_expected.to contain_class('mysql::server')
           .with_remove_default_accounts(true)
