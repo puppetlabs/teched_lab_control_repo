cc96df75886fa32fff64c11d0f8600bfa5608fec
Jain Waldrip <jain.waldrip@perforce.com>
2024-09-12 10:23:44 -0500

    Fix classification

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ File { backup => false }
 
 node default {
   # Classify nodes by the 'pp_role' fact if it exists
-  if $trusted['pp_role'] {
+  if $trusted['pp_role'].length() > 0 {
     include "role::${trusted['extensions']['pp_role']}"
   } else {
     include role::default
